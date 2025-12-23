9519d266eeefffa7631dbc4862b86ac03424f893
viktoriyakazunka-hash <viktoriya.kazunka@neuralab.tech>
2025-12-23 12:51:05 +0300

    Update birthday.js

--- birthday.js
+++ birthday.js
@@ -1,2 +1,35 @@
-postMessage("🧪 Тестовое сообщение от birthday-bot");
+const https = require("https");
 
+const token = process.env.SLACK_TOKEN;
+const channel = process.env.CHANNEL_ID;
+
+console.log("TOKEN EXISTS:", !!token);
+console.log("CHANNEL:", channel);
+
+const data = JSON.stringify({
+  channel: channel,
+  text: "🧪 Тестовое сообщение от birthday-bot"
+});
+
+const options = {
+  hostname: "slack.com",
+  path: "/api/chat.postMessage",
+  method: "POST",
+  headers: {
+    "Authorization": `Bearer ${token}`,
+    "Content-Type": "application/json",
+    "Content-Length": data.length
+  }
+};
+
+const req = https.request(options, res => {
+  let body = "";
+  res.on("data", chunk => body += chunk);
+  res.on("end", () => {
+    console.log("Slack response:", body);
+  });
+});
+
+req.on("error", err => console.error(err));
+req.write(data);
+req.end();
